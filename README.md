2e30b68c829904f5b8fc8e6819539cbe7d3f70a6
Hazem Khaled <hazem.khaled@gmail.com>
2015-12-23 13:45:53 +0200

    Everything already done

---
+++ README.md
@@ -0,0 +1,16 @@
+# TiWidowStack
+Manage iPhone and Android windows in same code base
+
+## Get it [![gitTio](http://gitt.io/badge.svg)](http://gitt.io/component/TiWidowStack)
+Download the latest distribution [ZIP-file](https://github.com/HazemKhaled/TiWidowStack/releases) and consult the [Titanium Documentation](http://docs.appcelerator.com/titanium/latest/#!/guide/Using_a_Module) on how install it, or simply use the [gitTio CLI](http://gitt.io/cli):
+
+`$ gittio install TiWidowStack`
+
+## How to
+Soon
+
+## Contributions
+Your issues and pull requests are most welcome..
+
+### Changelog
+**v1.0.0**<br>1st version with home, back and destroy methods
---
+++ WindowStack.js
@@ -0,0 +1,118 @@
+function WindowStack() {
+
+	// The navigation object for iOS
+	var navigationWindow = null,
+		// Windows array for history pepuse
+		windows = [],
+		that = this;
+
+	this.setNavigationWindow = function(_navigationWindow) {
+		navigationWindow = _navigationWindow;
+	};
+
+	this.open = function(_window, drawer) {
+
+		if (OS_IOS) {
+
+			// Create navigationWindow if we don't have, or if we have side menu
+			if (navigationWindow === null || drawer) {
+				navigationWindow = Ti.UI.iOS.createNavigationWindow({
+					window: _window
+				});
+
+				if (drawer) {
+					drawer.setCenterWindow(navigationWindow);
+				} else {
+					navigationWindow.open();
+				}
+
+				// Reset our local stack refrance
+				windows = [];
+			} else {
+
+				// Or just push new window to the stack
+				navigationWindow.openWindow(_window);
+
+				// Add this window to my stack refrance
+				windows.push(_window);
+			}
+		} else {
+
+			if (drawer) {
+				// Extend some properties to drawer containner
+				_.extend(drawer.window, _.pick(_window, ['title', 'keepScreenOn', 'exitOnClose']));
+
+				// Since android center item is view not a window, we have to fire it ourselves
+				_window.fireEvent('open');
+
+				drawer.setCenterWindow(_window);
+
+				// Reset our local stack refrance
+				windows = [];
+			} else {
+				_window.open();
+
+				// Add this window to my stack refrance
+				windows.push(_window);
+			}
+		}
+
+		// On close the window update the windows array
+		_window.addEventListener('close', function() {
+			windows = _.without(windows, _window);
+		});
+	};
+
+	this.close = function(_window) {
+
+		if (OS_IOS) {
+			navigationWindow.closeWindow(_window);
+		} else {
+			_window.close();
+		}
+	};
+
+	this.back = function() {
+		// Get last window in the stack
+		var _window = _.last(windows);
+
+		// In case assign this function directly to UI item, this will pass to the UI item it self, better use that
+		that.close(_window);
+	};
+
+	this.home = function() {
+		var lastLength = windows.length,
+			interval;
+
+		Alloy.Globals.homeInterval = interval = setInterval(function() {
+			if (lastLength === windows.length) {
+				Alloy.Globals.windowStack.back();
+				lastLength--;
+
+				if (lastLength === 0 ||
+					// Center window is actually view on Android
+					windows[lastLength - 1].apiName === 'View') {
+
+					clearInterval(Alloy.Globals.homeInterval);
+				}
+			}
+		}, 100);
+	};
+
+	// Close all screens, close the navigationWindow
+	this.destroy = function() {
+		if (OS_IOS) {
+			navigationWindow.close();
+		} else {
+			_.each(windows, function(_window) {
+				_window.close();
+			});
+		}
+	};
+
+}
+
+// Return new instance
+exports.createWindowStack = function() {
+	return new WindowStack();
+};
